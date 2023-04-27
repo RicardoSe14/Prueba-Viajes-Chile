8f691ebd96b24a96ecca0499d09ccd428c1f1136
RicardoSe14 <Ricardo.s14@gmail.com>
2023-04-26 20:44:37 -0400

    Revisión final

--- assets/js/ViajesChile.js
+++ assets/js/ViajesChile.js
@@ -3,19 +3,19 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-$(".card-title1").click(function(){
+$("#card-title1").click(function(){
     $("#descripcion1").toggle(100);
 });
 
-$(".card-title2").click(function(){
+$("#card-title2").click(function(){
     $("#descripcion2").toggle(100);
 });
 
-$(".card-title3").click(function(){
+$("#card-title3").click(function(){
     $("#descripcion3").toggle(100);
 });
 
-$(".card-title4").click(function(){
+$("#card-title4").click(function(){
     $("#descripcion4").toggle(100);
 });
 
